3e3f66de98c06656a9dd69312db08f6fbb7cb271
Eleneko <claudiasotopin@gmail.com>
2024-07-24 01:29:41 -0400

    wip

--- src/components/organisms/Information/Information.tsx
+++ src/components/organisms/Information/Information.tsx
@@ -6,12 +6,32 @@ export interface InformationProps {
   children?: React.ReactNode
 }
 
-const Information: React.FC<InformationProps> = ({ children }) => {
+const Information: React.FC<InformationProps> = () => {
 
   return (
-    <div className="dse-information">
-      {children}
+    <div className="container">
+    {/* Fila 1: Texto a la izquierda, imagen a la derecha */}
+    <div className="row">
+      <div className="text-left">
+        <h2>Suaje plano</h2>
+        <p>Este es un párrafo de ejemplo que está alineado a la izquierda.</p>
+      </div>
+      <div className="image-right">
+        <img src="/images/your-image1.png" alt="Imagen 1" />
+      </div>
     </div>
+
+    {/* Fila 2: Imagen a la izquierda, texto a la derecha */}
+    <div className="row">
+      <div className="text-left">
+        <img src="/images/your-image2.png" alt="Imagen 2" />
+      </div>
+      <div className="image-right">
+        <h2>Suaje rotativo</h2>
+        <p>Este es un párrafo de ejemplo que está alineado a la derecha.</p>
+      </div>
+    </div>
+  </div>
   )
 }
 

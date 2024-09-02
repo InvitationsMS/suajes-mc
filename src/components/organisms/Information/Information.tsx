fd0d027e37a14a92764480f8c00a67a8a42f28aa
Claudia Soto <102755159+Eleneko@users.noreply.github.com>
2024-09-02 17:51:55 -0400

    feat: styles

--- src/components/organisms/Information/Information.tsx
+++ src/components/organisms/Information/Information.tsx
@@ -9,35 +9,54 @@ export interface InformationProps {
 const Information: React.FC<InformationProps> = () => {
 
   return (
-    <div className="container">
+    // <div className="container">
 
-      <div className="row">
-        
-        <div className="text-left">
-          <h2>Suaje plano</h2>
-          <p>El suaje rotativo utiliza un cilindro giratorio con cuchillas para cortar materiales de forma continua. Es ideal para producciones de gran volumen, como etiquetas y cajas de cartón.</p>
-        </div>
+    //   <div className="row">
 
-        <div className="image-right">
-          <img src="/images/img1.jpeg"  alt="Imagen 1" />
-        </div>
+    //     <div className="text-left">
+    //       <h2>Suaje plano</h2>
+    //       <p>El suaje rotativo utiliza un cilindro giratorio con cuchillas para cortar materiales de forma continua. Es ideal para producciones de gran volumen, como etiquetas y cajas de cartón.</p>
+    //     </div>
 
+    //     <div className="image-right">
+    //       <img src="/images/img1.jpeg"  alt="Imagen 1" />
+    //     </div>
+
+    //   </div>
+
+    //   <div className="row">
+
+    //     <div className="text-left">
+    //       <img src="/images/img2.jpeg" alt="Imagen 2" />
+    //     </div>
+
+    //     <div className="image-right">
+    //       <h2>Suaje rotativo</h2>
+    //       <p>El suaje rotativo utiliza un cilindro giratorio con cuchillas para cortar materiales de forma continua. Es ideal para producciones de gran volumen, como etiquetas y cajas de cartón.</p>
+    //     </div>
+
+    //   </div>
+
+    // </div>
+    <div className="suaje-section">
+    <div className="suaje-item">
+      <div className="suaje-content">
+        <h2>Suaje plano</h2>
+
+        <p>El suaje plano utiliza un cilindro giratorio con cuchillas para cortar materiales de forma continua. Es ideal para producciones de gran volumen, como etiquetas y cajas de cartón.</p>
       </div>
-
-      <div className="row">
-
-        <div className="text-left">
-          <img src="/images/img2.jpeg" alt="Imagen 2" />
-        </div>
-
-        <div className="image-right">
-          <h2>Suaje rotativo</h2>
-          <p>El suaje rotativo utiliza un cilindro giratorio con cuchillas para cortar materiales de forma continua. Es ideal para producciones de gran volumen, como etiquetas y cajas de cartón.</p>
-        </div>
-
-      </div>
-      
+      <img src="/images/img1.jpeg" alt="Suaje plano" className="suaje-image" />
     </div>
+
+    <div className="suaje-item">
+      <div className="suaje-content">
+      <h2>Suaje rotativo</h2>
+      <p>El suaje rotativo utiliza un cilindro giratorio con cuchillas para cortar materiales de forma continua. Es ideal para producciones de gran volumen, como etiquetas y cajas de cartón.</p>
+      </div>
+      <img src="/images/img1.jpeg" alt="Suaje rotativo" className="suaje-image" />
+    </div>
+  </div>
+
   )
 }
 

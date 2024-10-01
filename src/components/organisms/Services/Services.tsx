6b23ee8e992063e67b067108e36016d2dbacda7e
Claudia Soto <102755159+Eleneko@users.noreply.github.com>
2024-10-01 16:15:09 -0300

    styles(services): responsive cards

--- src/components/organisms/Services/Services.tsx
+++ src/components/organisms/Services/Services.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import './Services.scss'
 
-
 export interface ServicesProps {
   children?: React.ReactNode
 }
@@ -16,25 +15,25 @@ const Services: React.FC<ServicesProps> = () => {
       <div className='cards-container'>
 
         <div className='card'>
-          <img src="/images/laser-machine.png" alt="Logo" width="100"/>
-          <h2>Suajes en calado láser</h2>
+          <img src="/images/laser-machine.png" alt="Logo" width="100" />
+          <h2>Suajes en calado<br></br>láser</h2>
           <p>Máquinas láser de alta precisión para quemado de madera plana y rotativa.</p>
         </div>
 
         <div className='card'>
           <img src="/images/cutting-saw.png" alt="Logo" width="100" />
-          <h2>Suajes en calado manual</h2>
+          <h2>Suajes en calado<br></br>manual</h2>
           <p>14 años de experiencia con Suajes en calado manual, planos o rotativos.</p>
         </div>
 
         <div className='card'>
-          <img src="/images/cutting-saw2.png" alt="Logo" width="100"/>
-          <h2>Renavajeo y reparación</h2>
+          <img src="/images/cutting-saw2.png" alt="Logo" width="100" />
+          <h2>Renavajeo y<br></br>reparación</h2>
           <p>Servicios de reparación y renavajeo de Suajes, scored, navaja y perforado.</p>
         </div>
 
       </div>
-      
+
     </div>
   )
 }
